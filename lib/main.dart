4af6c971a64cbc2d168efef2c7cfb4b0931dd38b
fadywaheed <fadywaheed10@gnail.com>
2020-05-13 22:45:14 +0200

    add some features

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:delifood/categories_screen.dart';
+import 'package:delifood/category_meals_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,7 +9,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              body2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              title: TextStyle(
+                  fontSize: 20.0,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.bold))),
       title: 'Flutter Demo',
+      initialRoute: CategoriesScreen.routeName,
+      routes: {
+        CategoriesScreen.routeName: (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
